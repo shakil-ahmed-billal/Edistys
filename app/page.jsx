c8b2688db52c0c5973c471cdc2461323992c4c7d
shakil-ahmed-billal <itzxhakil@gmail.com>
2025-02-01 19:11:39 +0600

    Add Technology component to showcase finance technology features

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,15 @@
 import Banner from "@/components/banner/Banner";
 import Future from "@/components/future/Future";
 import Innovation from "@/components/Innovation/Innovation";
+import Technology from "@/components/technology/Technology";
 
 export default function Home() {
   return (
     <div className="">
-        <Banner/>
-        <Future/>
-        <Innovation></Innovation>
+      <Banner />
+      <Future />
+      <Innovation></Innovation>
+      <Technology />
     </div>
   );
 }
---
+++ components/technology/Technology.jsx
@@ -0,0 +1,26 @@
+
+const Technology = () => {
+    return (
+        <div>
+            <div className="max-w-[1200px] mx-auto mt-40">
+                <p className="text-lightBlue text-center font-bold">TECHNOLOGY BUILT FOR YOU</p>
+                <p className="text-5xl font-bold text-center py-5">The future of finance</p>
+                <div className="p-14 shadow-2xl flex rounded-2xl">
+                    <div className="w-6/12">
+                        <div className="pr-5">
+                            <p className="text-lightBlue font-bold text-lg">Secure and safe</p>
+                            <p className="font-bold my-10 text-5xl">Highly secure and safe</p>
+                            <p className="mb-5 font-bold">Discover unparalleled security trusted by financial institutions across the globe. Our applications are meticulously developed in compliance with international security standards, drawing on 20 years of technical expertise.</p>
+                            <p>Join over 40 esteemed Fls, each serving more than 200 million customers, and benefit from our secure, robust and reliable infrastructure.</p>
+                        </div>
+                    </div>
+                    <div className="w-6/12">
+                        <img className="w-full rounded-2xl" src='/technology/technology1.avif' alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Technology
